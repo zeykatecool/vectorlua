2228c3fc72648321ed5bc4354325c7a260ed81b4
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2024-08-15 16:53:31 +0300

    Update draw.lua

--- examples/draw.lua
+++ examples/draw.lua
@@ -1,7 +1,8 @@
 local vector = require("vector")
 local vector2 = vector.vector2
 
-local newV2 = vector2.new(1,1)
+local newV2 = vector2.new(5,15)
+local anotherV2 = vector2.new(10,20)
 
 vector.draw({
     width = 512,
@@ -9,5 +10,9 @@ vector.draw({
     {
         vector = newV2,
         color = {255, 0, 0, 255},
-    }
+    },
+    {
+        vector = anotherV2,
+        color = {0, 255, 0, 255},
+    },
 },"myVector.png")
