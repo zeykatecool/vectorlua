f6646f06b805f87e103534a7fa9728ab0d041fac
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2024-08-15 16:52:09 +0300

    Create all.lua

---
+++ examples/all.lua
@@ -0,0 +1,30 @@
+local vector = require("vector")
+local vector2 = vector.vector2
+local vector3 = vector.vector3
+
+local newV3 = vector3.new(1,1,1)
+local newV2 = vector2.new(1,1)
+
+--[[
+    Vector3:
+    v:normalize()
+    v:dot(Vector)
+    v:cross(Vector)
+    v:angleBetween(Vector)
+    v:length()
+    v:lerp(Vector, t)
+    v:reflect(Vector)
+    v:project(Vector)
+    v:rotate(angle, axis)
+
+    Vector2:
+    v:normalize()
+    v:dot(Vector)
+    v:length()
+    v:angleBetween(Vector)
+    v:lerp(Vector, t)
+    v:reflect(Vector)
+    v:project(Vector)
+    v:rotate(angle)
+
+]]
